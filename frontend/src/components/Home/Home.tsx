76149d9f51621f6cea76eed78a365b3fc1d80ac9
Eduardo Santos <edupaz077@gmail.com>
2025-02-28 01:26:40 -0300

    refac: removed comented lines in css files

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -5,26 +5,26 @@ import './Home.css';
 const Home = () => {
     useEffect(() => {
         gsap.to('.star', {
-            y: "-=40",                // Movimenta a estrela um pouco para cima e para baixo
-            duration: 2,            // Tempo da animação
-            repeat: -1,               // Repete infinitamente
-            yoyo: true,               // Faz voltar ao tamanho original
-            stagger: 0.1, 
-            ease: "power1.inOut",      
+            y: "-=40",
+            duration: 2,
+            repeat: -1,
+            yoyo: true,
+            stagger: 0.1,
+            ease: "power1.inOut",
         });
         gsap.to('.star', {
-            scale: 1.8,               // Aumenta a estrela
-            rotationX: 200,           // Adiciona rotação no eixo X
-            rotationY: 200,           // Adiciona rotação no eixo Y
-            duration: 1.3,            // Tempo da animação
-            repeat: -1,               // Repete infinitamente
-            yoyo: true,               // Faz voltar ao tamanho original
-            ease: "power1.inOut",       // Suavização da animação
-            stagger: 0.2,             // Cada estrela começa um pouco depois da outra
-            transformOrigin: "center",// Garante que a rotação ocorra no centro da estrela
-            perspective: 500          // Cria a sensação de profundidade
+            scale: 1.8,
+            rotationX: 200,
+            rotationY: 200,
+            duration: 1.3,
+            repeat: -1,
+            yoyo: true,
+            ease: "power1.inOut",
+            stagger: 0.2,
+            transformOrigin: "center",
+            perspective: 500
         });
-        
+
     }, []);
 
 
