7653b13c5e101c735c4024b7eee496a450e1609c
Eduardo Santos <edupaz077@gmail.com>
2025-03-02 23:12:20 -0300

    refac: improved animations, added customLink and changed some colors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import LoadingScreen from "./components/loadingScreen/loadingScreen";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import SelectMusic from "./components/SelectMusic/SelectMusic";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -20,6 +21,7 @@ function App() {
       ) : (
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/musics" element={<SelectMusic />} />
         </Routes>
       )}
     </BrowserRouter>
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import gsap from 'gsap';
 import './Home.css';
+import CustomLink from '../../utils/CustomLink';
 
 const Home = () => {
     useEffect(() => {
@@ -9,7 +10,7 @@ const Home = () => {
             duration: 2,
             repeat: -1,
             yoyo: true,
-            stagger: 0.1,
+            stagger: 0.2,
             ease: "power1.inOut",
         });
         gsap.to('.star', {
@@ -24,9 +25,24 @@ const Home = () => {
             transformOrigin: "center",
             perspective: 500
         });
+        gsap.to('.star', {
+            scale: 1.8,
+            rotationX: 200,
+            rotationY: 200,
+            duration: 1.3,
+            repeat: -1,
+            yoyo: true,
+            ease: "power1.inOut",
+            stagger: 0.2,
+            transformOrigin: "center",
+            perspective: 500
+        });
+        gsap.fromTo(".Link",
+            { opacity: 0, scale: 0.7}, 
+            { opacity: 1, duration: 0.8, ease:"power3" ,stagger: 0.2, scale: 1, yoyo: true}
+        );
 
     }, []);
-
 
 
     return (
@@ -42,18 +58,18 @@ const Home = () => {
                 <img src="/star.svg" className="star star6" />
                 <img src="/star.svg" className="star star7" />
                 <img src="/star.svg" className="star star8" />
+                {/* <img src="/purpleStar.svg" className="purpleStar" />
+                <img src="/purpleStar.svg" className="purpleStar" /> */}
             </div>
 
             <div className='HomeButtons'>
                 <div>
-                    <button>
-                        Start Singing
-                    </button>
-                    <button>Start Singing</button>
+                    <CustomLink to='/musics' title='SELECT MUSIC' className='Link' />
+                    <CustomLink to='/musics' title='SELECT MUSIC' className='Link'/>
                 </div>
                 <div>
-                    <button>Start Singing</button>
-                    <button>Start Singing</button>
+                    <CustomLink to='/musics' title='SELECT MUSIC' className='Link' />
+                    <CustomLink to='/musics' title='SELECT MUSIC' className='Link' />
                 </div>
             </div>
         </div>
