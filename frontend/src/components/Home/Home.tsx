a10c7fd22fbea653c81260288b5fd81895a22738
Eduardo Santos <edupaz077@gmail.com>
2025-03-04 16:35:08 -0300

    refac: removed gsap from Home.tsx to animations.ts

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,49 +1,9 @@
-import { useEffect } from 'react';
-import gsap from 'gsap';
 import './Home.css';
 import CustomLink from '../../utils/CustomLink';
+import { animations } from './animations';
 
 const Home = () => {
-    useEffect(() => {
-        gsap.to('.star', {
-            y: "-=40",
-            duration: 2,
-            repeat: -1,
-            yoyo: true,
-            stagger: 0.2,
-            ease: "power1.inOut",
-        });
-        gsap.to('.star', {
-            scale: 1.8,
-            rotationX: 200,
-            rotationY: 200,
-            duration: 1.3,
-            repeat: -1,
-            yoyo: true,
-            ease: "power1.inOut",
-            stagger: 0.2,
-            transformOrigin: "center",
-            perspective: 500
-        });
-        gsap.to('.star', {
-            scale: 1.8,
-            rotationX: 200,
-            rotationY: 200,
-            duration: 1.3,
-            repeat: -1,
-            yoyo: true,
-            ease: "power1.inOut",
-            stagger: 0.2,
-            transformOrigin: "center",
-            perspective: 500
-        });
-        gsap.fromTo(".Link",
-            { opacity: 0, scale: 0.7}, 
-            { opacity: 1, duration: 0.8, ease:"power3" ,stagger: 0.2, scale: 1, yoyo: true}
-        );
-
-    }, []);
-
+   animations();
 
     return (
         <div className='Home'>
