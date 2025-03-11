5124a90002e7cebc15a180af926a38803c43cd14
Eduardo Santos <edupaz077@gmail.com>
2025-03-11 02:54:32 -0300

    feat: added a audio player

--- frontend/src/components/ViewMusic/ViewMusic.tsx
+++ frontend/src/components/ViewMusic/ViewMusic.tsx
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
-import { animations } from './animations'; // Importa a função de animação
+import { animations } from './animations';
 import './ViewMusic.css';
 import CustomLink from '../../utils/CustomLink';
+import musicTest2 from '../../Sounds/musicTest2.mp3';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 type ViewMusicProps = {
     musicName: string;
@@ -24,7 +27,14 @@ const ViewMusic = ({ musicName, author, duration }: ViewMusicProps) => {
                 <p>Author: {author}</p>
                 <p>Duration in secs: {duration}</p>
                 <div className='StartMusicBox'>
-                <CustomLink to="/select-music" title='START!' className='StartMusic' />
+                    <CustomLink to="/select-music" title='START!' className='StartMusic' />
+                    <AudioPlayer
+                        autoPlay
+                        src={musicTest2}
+                        onPlay={e => console.log("onPlay")}
+                        volume={0.3}
+                        style={{width: '100%', height:'100%'}}
+                    />
                 </div>
             </div>
         </div>
