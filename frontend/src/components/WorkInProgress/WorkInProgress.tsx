dde066684bcd7d7448f295fbe961cc43fe7a159b
Eduardo Santos <edupaz077@gmail.com>
2025-07-02 20:07:27 -0300

    fix: fix spelling

--- frontend/src/components/WorkInProgress/WorkInProgress.tsx
+++ frontend/src/components/WorkInProgress/WorkInProgress.tsx
@@ -5,7 +5,7 @@ const WorkInProgress = () => {
   return (
     <div className="WorkInProgress">
         <h1>This Page is not finished yet</h1>
-        <p>But maybe in a furute update it will be</p>
+        <p>But maybe in a future update it will be</p>
         <img src={`${import.meta.env.BASE_URL}/imgs/Chie/WorkInProgress.png`}/>
         <CustomLink to="/" title="Go back" className="Link PatchNotesGoBack"/>
     </div>
